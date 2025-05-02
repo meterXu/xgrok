f9660d06986b0fdb7662b40a539cabaed52e6717
isaac <xhgainxq@qq.com>
2025-05-02 14:15:43 +0800

    增加流量字段

--- assets/sql/update/sql.sql
+++ assets/sql/update/sql.sql
@@ -5,3 +5,12 @@ alter table ng_server
 alter table ng_server
     add operator varchar(50) null;
 
+-- 20250501 待更新
+alter table ng_server
+    add month_total_traffic int default 0 null comment '月总流量(MB)';
+
+alter table ng_server
+    add month_used_traffic int default 0 null comment '月已用流量(MB)';
+
+update ng_server set month_total_traffic=0 where month_total_traffic is null;
+update ng_server set month_used_traffic=0 where month_used_traffic is null;
