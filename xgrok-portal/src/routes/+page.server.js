6cea1ff991b4b4b61727244576ec6d34fe575eee
isaac <xhgainxq@qq.com>
2025-07-24 00:06:30 +0800

    Load environment variables from .env file based on NODE_ENV setting

--- xgrok-portal/src/routes/+page.server.js
+++ xgrok-portal/src/routes/+page.server.js
@@ -1,6 +1,11 @@
+import dotenv from 'dotenv'
+import path from 'path'
+dotenv.config({
+    path: path.resolve(`.env.${(process.env.NODE_ENV||'development').trim()}`)
+})
 export function load(){
     return new Promise((resolve,reject)=>{
-        const baseApi = import.meta.env.VITE_APP_baseApi
+        const baseApi = process.env.VITE_APP_baseApi
         fetch(`${baseApi}/version/latest`).then(res => res.json()).then((data) => {
            return resolve({
                version:data.name?.replace(/^v/gi,'')
