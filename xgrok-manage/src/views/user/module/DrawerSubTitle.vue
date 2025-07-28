838036731c7e298a9739d69cfb24adfa91ef32c8
isaac <xhgainxq@qq.com>
2025-07-28 22:54:18 +0800

    Add DrawerSubTitle component for loading indicators in UserTunnelDrawer
    - Create DrawerSubTitle.vue to display a title with loading and refresh icons.
    - Integrate DrawerSubTitle in UserTunnelDrawer.vue for WEB and service configuration sections.
    - Manage loading states with reactive properties for improved user experience.

---
+++ xgrok-manage/src/views/user/module/DrawerSubTitle.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {Loading, Refresh} from "@element-plus/icons-vue";
+const emits = defineEmits(["click"]);
+const props = defineProps(['loading'])
+</script>
+
+<template>
+  <h3 class="text-[14px] flex flex-row items-center gap-4">
+    <span><slot/></span>
+    <el-icon class="cursor-pointer hover:text-(--el-color-primary)!"
+             @click="()=>{loading?void(0):emits('click')}">
+      <Loading v-if="loading" class="is-loading"/>
+      <Refresh v-else/>
+    </el-icon>
+  </h3>
+</template>
+
+<style scoped lang="less">
+
+</style>
--- xgrok-manage/src/views/user/module/UserTunnelDrawer.vue
+++ xgrok-manage/src/views/user/module/UserTunnelDrawer.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import {useGetIndexMethod, usePage} from "@/libs/use-curd";
 import FillHeightContainer from "@/components/FillHeightContainer.vue";
-import {onMounted, reactive, shallowReactive, watch} from "vue";
+import {onMounted, reactive, shallowReactive, watch,ref} from "vue";
 import {getDict, queryTunnelWebConfig, tunnelServiceConfig} from "@/api";
 import {mappingDic, useFormatDic} from "@/libs/utils";
+import DrawerSubTitle from "@/views/user/module/DrawerSubTitle.vue";
 
 const isVisible = defineModel()
 const props = defineProps(['user'])
@@ -13,6 +14,8 @@ const tunnelWebPage = usePage()
 const tunnelServicePage = usePage()
 const serviceTypeDict = shallowReactive<DictItemType[]>([])
 const hostTypeDict = shallowReactive<DictItemType[]>([])
+const tunnelWebLoading = ref(false)
+const tunnelServiceLoading = ref(false)
 
 watch(() => isVisible.value, (nv) => {
   if(nv){
@@ -24,24 +27,30 @@ watch(() => isVisible.value, (nv) => {
 function onQueryTunnelWebConfig(pageNumber:number=1,pageSize:number=20){
   tunnelWebPage.pageNumber = pageNumber
   tunnelWebPage.pageSize = pageSize
+  tunnelWebLoading.value = true
   queryTunnelWebConfig({userId: props.user.id,...tunnelWebPage}).then((res) => {
     if(res.success&&res.data){
       tunnelWebConfigData.splice(0,tunnelWebConfigData.length);
       tunnelWebConfigData.push(...res.data.records as any []);
       tunnelWebPage.total = res.data.total;
     }
+  }).finally(() => {
+    tunnelWebLoading.value = false
   })
 }
 
 function onTunnelServiceConfig(pageNumber:number=1,pageSize:number=20){
   tunnelServicePage.pageNumber = pageNumber
   tunnelServicePage.pageSize = pageSize
+  tunnelServiceLoading.value = true
   tunnelServiceConfig({userId: props.user.id,...tunnelServicePage}).then((res) => {
     if(res.success&&res.data){
       tunnelServiceConfigData.splice(0,tunnelServiceConfigData.length);
       tunnelServiceConfigData.push(...res.data.records as any []);
       tunnelServicePage.total = res.data.total;
     }
+  }).finally(() => {
+    tunnelServiceLoading.value = false
   })
 }
 
@@ -68,12 +77,14 @@ onMounted(()=>{
     <template #default>
       <div class="w-full h-full flex flex-col justify-items-start items-start gap-8 text-[14px] ">
         <div class="flex-1/2 flex flex-col justify-items-start items-start gap-8 w-full">
-          <h3 class="text-[14px]">
+          <DrawerSubTitle :loading="tunnelWebLoading"
+                                 @click="()=>{onQueryTunnelWebConfig(tunnelWebPage.pageNumber,tunnelWebPage.pageSize)}">
             WEB配置
-          </h3>
+          </DrawerSubTitle>
           <FillHeightContainer>
             <el-table class="rounded-2xl!" height="100%" width="100%" header-row-class-name="table-header" row-key="id"
-            :data="tunnelWebConfigData">
+                      :load="tunnelWebLoading"
+                      :data="tunnelWebConfigData">
               <el-table-column fixed type="index" label="序号" align="center" :index="useGetIndexMethod" width="55"></el-table-column>
               <el-table-column prop="name" label="名称" align="left"></el-table-column>
               <el-table-column prop="hostname" show-overflow-tooltip width="100" label="设备名称" align="left"></el-table-column>
@@ -110,11 +121,13 @@ onMounted(()=>{
           </div>
         </div>
         <div class="flex-1/2 flex flex-col justify-items-start items-start gap-8 w-full">
-          <h3 class="text-[14px]">
+          <DrawerSubTitle :loading="tunnelServiceLoading"
+                                 @click="()=>{onTunnelServiceConfig(tunnelServicePage.pageNumber,tunnelServicePage.pageSize)}">
             服务配置
-          </h3>
+          </DrawerSubTitle>
           <FillHeightContainer>
             <el-table class="rounded-2xl!" height="100%" header-row-class-name="table-header" row-key="id"
+                      :load="tunnelServiceLoading"
                       :data="tunnelServiceConfigData">
               <el-table-column fixed type="index" label="序号" align="center" :index="useGetIndexMethod" width="55"></el-table-column>
               <el-table-column prop="name" label="名称" align="left"></el-table-column>
