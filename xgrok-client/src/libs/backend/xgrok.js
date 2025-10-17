9cf2e439423526c68458554d3576d7b69842b3a7
isaac <xhgainxq@qq.com>
2025-10-17 09:21:34 +0800

    feat: 在开发环境中记录xgrok配置文件信息
    在开发模式下，添加日志记录以输出xgrok配置文件的内容，便于调试和配置检查。

--- xgrok-client/src/libs/backend/xgrok.js
+++ xgrok-client/src/libs/backend/xgrok.js
@@ -118,6 +118,9 @@ function startXgrok(names,type) {
         global.logger.info(`os arch: ${arch()}`)
         global.logger.info(`os platform: ${platform()}`)
         global.logger.info(`start xgrok, the client root path is ${global.project.clientRootPath}`)
+        if(process.env.NODE_ENV==='development'){
+            global.logger.info(`config:\r\n${readXgrokCfgFile(global.project.xgrokCoreCfgPath)}`)
+        }
         let xgrok = null
         if(type===serverType.ngrok){
             xgrok = execFile('./xgrok-core', [`-config=${global.project.xgrokCoreCfgPath}`, 'start', ...names], {
