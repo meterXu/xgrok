3e04deb1a900c18309a0dc2254f999da4c371b40
isaac <xhgainxq@qq.com>
2025-12-15 21:12:04 +0800

    feat(web): 增加链接复制功能，支持点击复制HTTPS链接并显示成功通知

--- xgrok-client/view/pages/web/module/WebItem.vue
+++ xgrok-client/view/pages/web/module/WebItem.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TunnelItem from "@/components/tunnel/TunnelItem.vue";
 import {useMyTitle} from "@/libs/common";
-import {httpType} from "@/libs/enums";
+import {httpType, NotificationType} from "@/libs/enums";
 import {useAppStore} from "@/store";
 import {useClientTypeExecute} from "@/libs/useAction";
+import {doCopy} from 'xxweb-util'
+import {showNotification} from "@/libs/message";
 
 const props = defineProps(['item'])
 const store = useAppStore()
@@ -27,17 +29,26 @@ function onOpenLink(item, type) {
   })
 }
 
+function onCopy(item, type){
+  const link = httpUrl(item, type)
+  doCopy(link).then(res=>{
+    showNotification(NotificationType.success, '链接复制成功')
+  })
+}
+
 </script>
 
 <template>
   <TunnelItem class="status flex flex-col gap-4 mx-8"
               :id="item.id" :status="item.status" :online="item.is_online">
     <span class="overflow-hidden text-ellipsis">{{ useMyTitle(item) }}</span>
-    <span class="w-full flex items-center justify-between">
+    <span class="w-full flex items-center justify-center gap-6">
                 <span class="overflow-hidden text-ellipsis">{{ httpUrl(item, httpType.https) }}</span>
+                <el-tooltip content="点击复制">
+                  <IconParkOutlineCopy @click="onCopy(item,httpType.https)" class="text-[19px] hover:text-(--el-color-primary)"/>
+                </el-tooltip>
                 <el-tooltip content="点击访问">
-                  <IconParkOutlineEarth @click="onOpenLink(item,httpType.https)"
-                                        class="text-[16px] hover:text-(--el-color-primary)"/>
+                  <IconParkOutlineEarth @click="onOpenLink(item,httpType.https)" class="text-[19px] hover:text-(--el-color-primary)"/>
                 </el-tooltip>
               </span>
   </TunnelItem>
